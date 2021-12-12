b4e0c7efa3bd11635fe2b318529d8a9cabc759f7
prakharn111 <prakharnagore111@gmail.com>
2021-12-12 19:28:14 +0530

    Accordion  first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: "What is Lorem Ipsum?",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 2,
+    title: "What is Lorem Ipsum?",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 3,
+    title: "What is Lorem Ipsum?",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 4,
+    title: "What is Lorem Ipsum?",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 5,
+    title: "What is Lorem Ipsum?",
+    info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+];
+export default questions;
